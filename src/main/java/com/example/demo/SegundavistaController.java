76566376b8c75b77f3f3f56ab9f5641d33e1330e
CortezMelendez <20253RD043@utez.edu.mx>
2026-03-27 13:14:39 -0600

    tenemos el programa anterior, asi que partimos de ahi

--- src/main/java/com/example/demo/SegundavistaController.java
+++ src/main/java/com/example/demo/SegundavistaController.java
@@ -1,4 +1,50 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SegundavistaController {
+
+    @FXML
+    private TextField monto;
+
+    @FXML
+    private Label resultado;
+
+    private double saldo = 0;
+
+    @FXML
+    public void depositar() {
+        try {
+            double m = Double.parseDouble(monto.getText());
+            saldo += m;
+            resultado.setText("Depositado: " + m);
+        } catch (Exception e) {
+            resultado.setText("Ingresa un número válido");
+        }
+    }
+
+    @FXML
+    public void retirar() {
+        try {
+            double m = Double.parseDouble(monto.getText());
+
+            if (m <= saldo) {
+                saldo -= m;
+                resultado.setText("Retiro exitoso: " + m);
+            } else {
+                resultado.setText("Saldo insuficiente");
+            }
+
+        } catch (Exception e) {
+            resultado.setText("Ingresa un número válido");
+        }
+    }
+
+    @FXML
+    public void consultar() {
+        resultado.setText("Saldo actual: " + saldo);
+    }
 }
+//tqm
